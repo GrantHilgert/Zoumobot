b0f2509e44ce18549fcf7b22956c8b597c02deeb
Grant Hilgert <granthilgert@mail.missouri.edu>
2018-01-29 14:57:27 -0600

    Create zoumobot_extension.c

---
+++ bootloader/optiboot/zoumobot_extension.c
@@ -0,0 +1,17 @@
+/* Zoumobot Interupt and Boot mode extensions
+Written by Grant Hilgert
+
+This extensions created a development mode and competiton mode for the zoumobot.
+The mode is selected via an external DIP switch.
+
+In development mode, the device behaves like a normal arduinio, allowing code to be uploaded over the serial port.
+In compention mode, code can still be uplaoded over the serial port. However, the correct IR signal must be received 
+before execution is allowed to begin
+
+
+*/
+
+#define DIP0 0
+#define DIP1 0
+#define IR0 0
+#define IR1 0
