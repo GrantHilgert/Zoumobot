42b07ccd5de7859d76afc8b87960635fca4450a3
Grant Hilgert <granthilgert@mail.missouri.edu>
2018-01-29 23:06:34 -0600

    Added hardware serial

--- pins_arduino.h
+++ pins_arduino.h
@@ -32,30 +32,30 @@
 #define analogInputToDigitalPin(p)  ((p < 16) ? (p) + 54 : -1)
 #define digitalPinHasPWM(p)         (((p) >= 2 && (p) <= 13) || ((p) >= 44 && (p)<= 46))
 
-#define PIN_SPI_SS    (53)
-#define PIN_SPI_MOSI  (51)
-#define PIN_SPI_MISO  (50)
-#define PIN_SPI_SCK   (52)
+#define PIN_SPI_SS    (30)
+#define PIN_SPI_MOSI  (27)
+#define PIN_SPI_MISO  (28)
+#define PIN_SPI_SCK   (29)
 
 static const uint8_t SS   = PIN_SPI_SS;
 static const uint8_t MOSI = PIN_SPI_MOSI;
 static const uint8_t MISO = PIN_SPI_MISO;
 static const uint8_t SCK  = PIN_SPI_SCK;
 
-#define PIN_WIRE_SDA        (20)
-#define PIN_WIRE_SCL        (21)
+#define PIN_WIRE_SDA        (12)
+#define PIN_WIRE_SCL        (13)
 
 static const uint8_t SDA = PIN_WIRE_SDA;
 static const uint8_t SCL = PIN_WIRE_SCL;
 
 #define LED_BUILTIN 13
 
-#define PIN_A0   (54)
-#define PIN_A1   (55)
-#define PIN_A2   (56)
-#define PIN_A3   (57)
-#define PIN_A4   (58)
-#define PIN_A5   (59)
+#define PIN_A0   (7)
+#define PIN_A1   (8)
+#define PIN_A2   (9)
+#define PIN_A3   (10)
+#define PIN_A4   (11)
+#define PIN_A5   (12)
 
 
 static const uint8_t A0 = PIN_A0;
@@ -65,12 +65,12 @@ static const uint8_t A3 = PIN_A3;
 static const uint8_t A4 = PIN_A4;
 static const uint8_t A5 = PIN_A5;
 
-#define PIN_D0   (54)
-#define PIN_D1   (55)
-#define PIN_D2   (56)
-#define PIN_D3   (57)
-#define PIN_D4   (58)
-#define PIN_D5   (59)
+#define PIN_D0   (1)
+#define PIN_D1   (2)
+#define PIN_D2   (3)
+#define PIN_D3   (4)
+#define PIN_D4   (5)
+#define PIN_D5   (6)
 
 
 static const uint8_t D0 = PIN_D0;
@@ -281,11 +281,5 @@ const uint8_t PROGMEM digital_pin_to_timer_PGM[] = {
 //                            pins are NOT connected to anything by default.
 #define SERIAL_PORT_MONITOR         Serial
 #define SERIAL_PORT_HARDWARE        Serial
-#define SERIAL_PORT_HARDWARE1       Serial1
-#define SERIAL_PORT_HARDWARE2       Serial2
-#define SERIAL_PORT_HARDWARE3       Serial3
-#define SERIAL_PORT_HARDWARE_OPEN   Serial1
-#define SERIAL_PORT_HARDWARE_OPEN1  Serial2
-#define SERIAL_PORT_HARDWARE_OPEN2  Serial3
 
 #endif
